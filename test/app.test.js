2df5730aa013aadc23767653f554840e73d2a5f4
Rafael Matos <rafaelmatos@fsbtech.com>
2024-08-30 10:23:36 -0300

    adjested server initialization

--- test/app.test.js
+++ test/app.test.js
@@ -1,13 +1,20 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const { expect } = chai;
-const app = require('../app')
+const app = require('../app');
+let server;
 
 chai.use(chaiHttp);
 
 describe('New API', () => {
+    before((done) => {
+        server = app.listen(8000, done);
+    });
+    after((done) => {
+        server.close(done);
+    });
     it('Should return a welcome message on the root route', (done) => {
-        chai.request(app)
+        chai.request(server)
             .get('/')
             .end((err, res) => {
                 expect(res).to.have.status(200);
@@ -17,7 +24,7 @@ describe('New API', () => {
     });
 
     it('Should return an array of news articles on /v1/news', (done) => {
-        chai.request(app)
+        chai.request(server)
             .get('/v1/news')
             .end((err, res) => {
                 expect(res).to.have.status(200);
