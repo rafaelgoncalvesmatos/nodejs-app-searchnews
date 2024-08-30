f897c31da336444c3d8acd7d929903a02e2a8342
Rafael Matos <rafaelmatos@fsbtech.com>
2024-08-30 10:03:00 -0300

    fix the path for app-test

---
+++ app/app.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = chai;
+const app = require('../app')
+
+chai.use(chaiHttp);
+
+describe('New API', () => {
+    it('Should return a welcome message on the root route', (done) => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.equal(`Welcome Rafael to my Search New API http://localhost:8080/v1/news`);
+                done();
+            });
+    });
+
+    it('Should return an array of news articles on /v1/news', (done) => {
+        chai.request(app)
+            .get('/v1/news')
+            .end((err, res) => {
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.an('array');
+                expect(res.body.length).to.be.greaterThan(0);
+                done();
+            });
+    });
+});
+
